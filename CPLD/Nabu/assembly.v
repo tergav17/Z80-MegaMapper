16044401b7883f5cd1ace05b7b73b2e4610efca6
Gavin Tersteeg <gctersteeg@gmail.com>
2024-05-16 22:07:24 -0500

    Continued work on CPLD verilog

---
+++ CPLD/Nabu/assembly.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    02:45:57 05/17/2024 
+// Design Name: 
+// Module Name:    assembly 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module assembly(
+    inout [7:0] data,
+    input wr_n,
+    input rd_n,
+    input iorq_n,
+    input mreq_n,
+    input m1_n,
+    input [7:0] addr,
+    input reset_n,
+    output iorq_sys_n,
+    output mreq_sys_n
+    );
+
+
+endmodule
--- CPLD/Nabu/registers.v
+++ CPLD/Nabu/registers.v
@@ -25,13 +25,18 @@ module registers(
     input iorq_n,
     input m1_n,
 	 input record_isr,
-	 input reset_n
+	 input reset_n,
+	 output [7:0] ctrl_out
     );
 
 // Define control and instruction registers
 reg[7:0] ctrl_reg;
 reg[7:0] isr_reg;
 
+// Logic for reading the control register
+assign ctrl_out = ctrl_reg;
+
+// Logic for controlling the contents of the control register
 always @(posedge wr_n or negedge reset_n)
 begin
 	if (!reset_n)
@@ -40,6 +45,10 @@ begin
 		ctrl_reg = data;
 end
 
+// Logic for reading the output register
+assign data = (!iorq_n && !rd_n) ? isr_reg : 8'bZZZZZZZZ;
+
+// Logic for controlling the contents of the instruction registers
 always @(posedge m1_n)
 begin
 	if (record_isr)
