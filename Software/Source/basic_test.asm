27b444a96764a80fa3502725a72ac40076ca6f33
tergav17 <gctersteeg@gmail.com>
2024-07-08 23:52:14 -0500

    Started work on ZMM basic test

---
+++ Software/Source/basic_test.asm
@@ -0,0 +1,13 @@
+;
+;**************************************************************
+;*
+;*      Z M M   B A S I C   F U N C T I O N   T E S T
+;*
+;*      Tests memory and basic functionality of the ZMM
+;*      (Z80 MegaMapper). Segmentation as well as I/O
+;*      traps.
+;* 
+;**************************************************************
+
+; Program start
+	org	0x0100
