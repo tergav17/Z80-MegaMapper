ffd0e253dfbaa239a63bb69a0e86180446b23e50
tergav17 <gctersteeg@gmail.com>
2024-07-10 23:54:03 -0500

    Switched untrap logic to use RETN

--- CPLD/Nabu/assembly.v
+++ CPLD/Nabu/assembly.v
@@ -1,38 +1,70 @@
 `timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
+
 // Company: 
+
 // Engineer: Gavin Tersteeg
+
 // 
+
 // Create Date:    02:45:57 05/17/2024 
+
 // Design Name: 
+
 // Module Name:    assembly 
+
 // Project Name: 
+
 // Target Devices: 
+
 // Tool versions: 
+
 // Description: 
+
 //
+
 // Dependencies: 
+
 //
+
 // Revision: 
+
 // Revision 0.01 - File Created
+
 // Additional Comments: 
+
 //
+
 //////////////////////////////////////////////////////////////////////////////////
+
 module assembly(
+
     inout [7:0] data,
+
     input wr_n,
+
     input rd_n,
+
     input iorq_n,
+
     input mreq_n,
     input irq_sys_n,
+
     input m1_n,
+
     input refresh_n,
+
     input [2:0] lo_addr,
     input [1:0] hi_addr,
     input io_enable,
+
     input reset_n,
+
     output iorq_sys_n,
+
     output mreq_sys_n,
+
     output irq_n,
     output nmi_n,
     output trans_addr,
@@ -43,17 +75,29 @@ module assembly(
     output xmem_sel_n,
     output trans_wr_n,
     output trans_direction
+
     );
+
+
 
 wire [2:0] ctrl_register;
 
+
+
 // Define activation condition for the mapper I/O space
+
 wire mapper_io = io_enable && !iorq_n;
 
+
+
 // Define activation condition for reading the instruction register
+
 wire read_isr_en = mapper_io && !lo_addr[2] && !lo_addr[1] && !lo_addr[0];
 
+
+
 // Define activation condition for writing the control register
+
 wire write_ctrl_en = mapper_io && lo_addr[2] && !lo_addr[1]; 
 
 // Define I/O violation condition
@@ -61,7 +105,7 @@ wire io_violation_cond = mapper_io && lo_addr[2] && lo_addr[1];
 
 // Keep track of instruction state going into mode logic
 wire new_isr;
-wire last_isr_jmp;
+wire last_isr_untrap;
 
 // Trap state used to control how interrupts work
 wire trap_state;
@@ -84,11 +128,13 @@ assign trap_addr_rd_n = !(mapper_io && !lo_addr[2] && lo_addr[1]) || rd_n;
 assign bank_wr_n = !(mapper_io && !lo_addr[2]) || wr_n;
 
 // Suppress I/O when in mapper I/O space
+
 assign iorq_sys_n = iorq_n || mapper_io;
 
 // Supress memory accesses when external memory or writing to the translation table
 // If a refresh operation occures or virtualization is turned off, no remapping should be done
 wire mreq_override_cond = (trap_state ? hi_addr[1] : 1) && refresh_n && virtual_enable;
+
 assign mreq_sys_n = mreq_override_cond || mreq_n;
 assign trans_wr_n = !mreq_override_cond || mreq_n || !(trap_state && !hi_addr[0]) || wr_n;
 assign xmem_sel_n = !mreq_override_cond || mreq_n || (trap_state && !hi_addr[0]);
@@ -108,13 +154,21 @@ assign trans_direction = mreq_n ? io_direction : set_trans_direction;
 // I/O violations need to be readable from the instruction register
 wire io_violation_occured;
 
+
 // Create instance of register logic
+
 registers reg_0(data, wr_n, rd_n, m1_n, !trap_state, read_isr_en, write_ctrl_en, reset_n, io_violation_occured, ctrl_register);
 
+
+
 // Create instance of opcode detection logic
-opcode opcode_0(data, m1_n, capture_addr, new_isr, last_isr_jmp, io_direction);
+
+opcode opcode_0(data, m1_n, capture_addr, new_isr, last_isr_untrap, io_direction);
 
 // Create instance of mode logic
-modes modes_0(io_violation_cond, irq_sys_n, m1_n, new_isr, last_isr_jmp, virtual_enable, io_violation_occured, trap_state, nmi_n, capture_addr);
+modes modes_0(io_violation_cond, irq_sys_n, m1_n, new_isr, last_isr_untrap, virtual_enable, io_violation_occured, trap_state, nmi_n, capture_addr);
+
+
 
 endmodule
+
--- CPLD/Nabu/modes.v
+++ CPLD/Nabu/modes.v
@@ -1,42 +1,74 @@
 `timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
+
 // Company: 
+
 // Engineer: 
+
 // 
+
 // Create Date:    04:41:31 05/19/2024 
+
 // Design Name: 
+
 // Module Name:    modes 
+
 // Project Name: 
+
 // Target Devices: 
+
 // Tool versions: 
+
 // Description: 
+
 //
+
 // Dependencies: 
+
 //
+
 // Revision: 
+
 // Revision 0.01 - File Created
+
 // Additional Comments: 
+
 //
+
 //////////////////////////////////////////////////////////////////////////////////
+
 module modes(
+
     input io_violation,
+
     input irq_sys_n,
+
     input m1_n,
+
     input new_isr,
-    input last_isr_jmp,
+
+    input last_isr_untrap,
     input virtual_enabled,
     output io_violation_occured,
     output trap_state,
+
     output nmi_n,
+
     output capture_address
+
     );
 
 
+
+
 // Flip-flop to keep track of if a trap is currently happening 
+
 reg trap_state_r;
 
 // Has an I/O address violation occured?
 reg io_violation_occured_r;
+
 
 // Address capture latch
 reg capture_latch_r;
@@ -46,7 +78,7 @@ reg irq_sync_r;
 
 // Assign registers to outputs
 assign trap_state = trap_state_r;
-assign capture_address = capture_latch_r;
+assign capture_address = capture_latch_r || (last_isr_untrap && trap_state && virtual_enabled);
 assign io_violation_occured = io_violation_occured_r;
 
 // A trap can said to be pending when either there is an interrupt waiting, or a I/O violation has been observed
@@ -81,16 +113,23 @@ begin
    end 
    else begin
       // Trap can be ended by executing a jump instruction
-      if (last_isr_jmp && virtual_enabled)
+      if (last_isr_untrap && virtual_enabled)
          trap_state_r = 0;
    end
 end
 
+
 always @(posedge m1_n)
+
 begin
    // Interrupt gets updated on the positive edge of every M1 cycle
    // May slow down interrupt response by an instruction or two, but gets rid of a lot of edge cases
+
    irq_sync_r = irq_sys_n;
+
 end
 
+
+
 endmodule
+
