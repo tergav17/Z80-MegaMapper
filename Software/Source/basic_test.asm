5993b5e8af111f60c571d71b302bdac3649b31f8
tergav17 <gctersteeg@gmail.com>
2024-07-15 23:56:43 -0500

    Continued work on ZMM test

--- Software/Source/basic_test.asm
+++ Software/Source/basic_test.asm
@@ -16,6 +16,10 @@ b_coin	equ	0x01
 b_cout	equ	0x02
 b_print	equ	0x09
 
+zm_ctrl	equ	0x34
+
+zm_map	equ	0x8000
+
 ; Program start
 	org	0x0100
 	
@@ -24,6 +28,21 @@ start:	di
 	ld	c,b_print
 	ld	de,splash
 	call	bdos
+	
+	; Set up passthru table for mapper mode
+	ld	a,0b00000001
+	out	(zm_ctrl),a
+	ld	hl,zm_map
+gentab0:ld	(hl),l
+	inc	l
+	jp	nz,gentab0
+	ld	a,0b00000101
+	out	(zm_ctrl),a
+	ld	hl,zm_map
+gentab0:ld	(hl),l
+	inc	l
+	jp	nz,gentab0
+	
 
 
 ; Strings
